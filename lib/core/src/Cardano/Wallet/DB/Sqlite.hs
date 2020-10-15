bec57c9d6405be7a2988dfcb748d6a5128c1044b
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2020-10-15 16:05:49 +1000

    Get epoch and slot lengths from HFC History Interpreter
    Divide up network parameters further.
    
    Remove slot length and epoch length from genesis parameters, because
    we will be getting them with HFC queries.
    
    Update all slotting parameter usages

--- lib/core/src/Cardano/Wallet/DB/Sqlite.hs
+++ lib/core/src/Cardano/Wallet/DB/Sqlite.hs
@@ -156,7 +156,7 @@ import Data.Text.Class
 import Data.Typeable
     ( Typeable )
 import Data.Word
-    ( Word16, Word32, Word64 )
+    ( Word16, Word32 )
 import Database.Persist.Class
     ( toPersistValue )
 import Database.Persist.Sql
@@ -1079,11 +1079,10 @@ mkCheckpointEntity wid wal =
         , checkpointBlockHeight = bh
         , checkpointGenesisHash = BlockId (coerce (gp ^. #getGenesisBlockHash))
         , checkpointGenesisStart = coerce (gp ^. #getGenesisBlockDate)
-        , checkpointSlotLength = coerceSlotLength $ gp ^. #getSlotLength
-        , checkpointEpochLength = coerce (gp ^. #getEpochLength)
+        , checkpointSlotLength = 0
+        , checkpointEpochLength = 0
         , checkpointEpochStability = coerce (gp ^. #getEpochStability)
-        , checkpointActiveSlotCoeff =
-            W.unActiveSlotCoefficient (gp ^. #getActiveSlotCoefficient)
+        , checkpointActiveSlotCoeff = 0
         , checkpointFeePolicyUnused = ""
         , checkpointTxMaxSizeUnused = 0
         }
@@ -1092,9 +1091,6 @@ mkCheckpointEntity wid wal =
         | (W.TxIn input ix, W.TxOut addr coin) <- utxoMap
         ]
     utxoMap = Map.assocs (W.getUTxO (W.utxo wal))
-
-    coerceSlotLength :: W.SlotLength -> Word64
-    coerceSlotLength (W.SlotLength x) = toEnum (fromEnum x)
 
 -- note: TxIn records must already be sorted by order
 -- and TxOut records must already by sorted by index.
@@ -1115,11 +1111,11 @@ checkpointFromEntity cp utxo s =
         (BlockId genesisHash)
         genesisStart
         _feePolicyUnused
-        slotLength
-        epochLength
+        _slotLengthUnused
+        _epochLengthUnused
         _txMaxSizeUnused
         epochStability
-        activeSlotCoeff
+        _activeSlotCoeffUnused
         ) = cp
     header = (W.BlockHeader slot (Quantity bh) headerHash parentHeaderHash)
     utxo' = W.UTxO . Map.fromList $
@@ -1129,10 +1125,7 @@ checkpointFromEntity cp utxo s =
     gp = W.GenesisParameters
         { getGenesisBlockHash = coerce genesisHash
         , getGenesisBlockDate = W.StartTime genesisStart
-        , getSlotLength = W.SlotLength (toEnum (fromEnum slotLength))
-        , getEpochLength = W.EpochLength epochLength
         , getEpochStability = Quantity epochStability
-        , getActiveSlotCoefficient = W.ActiveSlotCoefficient activeSlotCoeff
         }
 
 mkTxHistory

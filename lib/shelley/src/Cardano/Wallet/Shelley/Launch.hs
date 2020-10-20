92c4af1048007a0a3cc3c5e12da65fb96dcbd0cd
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2020-10-20 14:08:21 +1000

    Set umask for integration tests
    So that cardano-node does not complain about file permissions.

--- lib/shelley/src/Cardano/Wallet/Shelley/Launch.hs
+++ lib/shelley/src/Cardano/Wallet/Shelley/Launch.hs
@@ -84,6 +84,8 @@ import Cardano.Launcher.Node
     , NodePort (..)
     , withCardanoNode
     )
+import Cardano.Startup
+    ( restrictFileMode )
 import Cardano.Wallet.Api.Server
     ( Listen (..) )
 import Cardano.Wallet.Logging
@@ -184,8 +186,6 @@ import System.IO.Temp
     ( createTempDirectory, getCanonicalTemporaryDirectory, withTempDirectory )
 import System.IO.Unsafe
     ( unsafePerformIO )
-import System.Posix.Files
-    ( ownerReadMode, setFileMode )
 import System.Process
     ( readProcess, readProcessWithExitCode )
 import Test.Utils.Paths
@@ -631,7 +631,7 @@ withBFTNode tr baseDir params action =
         let copyKeyFile f = do
                 let dst = dir </> f
                 copyFile (source </> f) dst
-                setFileMode dst ownerReadMode
+                restrictFileMode dst
                 pure dst
 
         [bftCert, bftPrv, vrfPrv, kesPrv, opCert] <- forM

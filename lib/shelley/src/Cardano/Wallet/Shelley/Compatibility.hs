971ac28c0caf3855c73f3264a792e3a63fc7bf87
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2020-10-21 09:56:29 +1000

    Use Cardano.Api.Shelley.fromShelleyMetaData instead of own

--- lib/shelley/src/Cardano/Wallet/Shelley/Compatibility.hs
+++ lib/shelley/src/Cardano/Wallet/Shelley/Compatibility.hs
@@ -102,6 +102,8 @@ import Cardano.Address.Derivation
     ( XPub, xpubPublicKey )
 import Cardano.Address.Style.Shelley
     ( inspectShelleyAddress )
+import Cardano.Api.Shelley
+    ( fromShelleyMetaData )
 import Cardano.Api.Shelley.Genesis
     ( ShelleyGenesis (..) )
 import Cardano.Api.Typed
@@ -704,7 +706,7 @@ fromShelleyTx (SL.Tx bod@(SL.TxBody ins outs certs wdrls _ _ _ _) _ mmd) =
         (map ((,W.Coin 0) . fromShelleyTxIn) (toList ins))
         (map fromShelleyTxOut (toList outs))
         (fromShelleyWdrl wdrls)
-        (fromShelleyMetadata <$> SL.strictMaybeToMaybe mmd)
+        (fromShelleyMD <$> SL.strictMaybeToMaybe mmd)
     , mapMaybe fromShelleyDelegationCert (toList certs)
     , mapMaybe fromShelleyRegistrationCert (toList certs)
     )
@@ -713,17 +715,8 @@ fromShelleyWdrl :: SL.Wdrl crypto -> Map W.ChimericAccount W.Coin
 fromShelleyWdrl (SL.Wdrl wdrl) = Map.fromList $
     bimap (fromStakeCredential . SL.getRwdCred) fromShelleyCoin <$> Map.toList wdrl
 
-fromShelleyMetadata :: SL.MetaData -> Cardano.TxMetadata
-fromShelleyMetadata (SL.MetaData md) = Cardano.makeTransactionMetadata md'
-  where
-    md' = fmap convert md
-    convert :: SL.MetaDatum -> Cardano.TxMetadataValue
-    convert = \case
-        SL.I n -> Cardano.TxMetaNumber n
-        SL.B bs -> Cardano.TxMetaBytes bs
-        SL.S s -> Cardano.TxMetaText s
-        SL.Map as -> Cardano.TxMetaMap (bimap convert convert <$> as)
-        SL.List xs -> Cardano.TxMetaList (map convert xs)
+fromShelleyMD :: SL.MetaData -> Cardano.TxMetadata
+fromShelleyMD = Cardano.makeTransactionMetadata . fromShelleyMetaData
 
 -- Convert & filter Shelley certificate into delegation certificate. Returns
 -- 'Nothing' if certificates aren't delegation certificate.
